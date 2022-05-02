504db3b01145b1f9757cbcb6c32728d88299c821
Mathias Kristensen <mathiaskristensen52@gmail.com>
2022-05-02 12:18:48 +0200

    Initial commit

---
+++ app/src/main/java/easv/oe/mcamera1/MainActivity.kt
@@ -0,0 +1,161 @@
+package easv.oe.mcamera1
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RelativeLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "xyz"
+    private val PERMISSION_REQUEST_CODE = 1
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE = 101
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_BITMAP = 102
+
+    var mFile: File? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        checkPermissions()
+
+    }
+
+
+    fun onTakeByFile(view: View) {
+        mFile = getOutputMediaFile("Camera01") // create a file to save the image
+
+        if (mFile == null) {
+            Toast.makeText(this, "Could not create file...", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        // create Intent to take a picture
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        // Add extra to inform the app where to put the image.
+        val applicationId = "easv.oe.mcamera1"
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, FileProvider.getUriForFile(
+                this,
+                "${applicationId}.provider",  //use your app signature + ".provider"
+                mFile!!))
+
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE)
+
+    }
+
+    fun onTakeByBitmap(view: View) {
+        if (isGranted(Manifest.permission.CAMERA)) {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_BITMAP)
+        } else {
+
+        }
+    }
+
+
+
+    //Checks if the app has the required permissions, and prompts the user with the ones missing.
+    private fun checkPermissions() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return
+        val permissions = mutableListOf<String>()
+        if ( ! isGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE) ) permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        if ( ! isGranted(Manifest.permission.CAMERA) ) permissions.add(Manifest.permission.CAMERA)
+        if (permissions.size > 0)
+            ActivityCompat.requestPermissions(this, permissions.toTypedArray(), PERMISSION_REQUEST_CODE)
+    }
+
+
+    private fun isGranted(permission: String): Boolean =
+            ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+
+
+    // return a new file with a timestamp name in a folder named [folder] in
+    // the external directory for pictures.
+    // Return null if the file cannot be created
+    private fun getOutputMediaFile(folder: String): File? {
+        // in an emulated device you can see the external files in /sdcard/Android/data/<your app>.
+          val mediaStorageDir = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), folder)
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(TAG, "failed to create directory")
+                return null
+            }
+        }
+
+        // Create a media file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val postfix = "jpg"
+        val prefix = "IMG"
+        return File(mediaStorageDir.path +
+                File.separator + prefix +
+                "_" + timeStamp + "." + postfix)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val mImage = findViewById<ImageView>(R.id.imgView)
+        val tvImageInfo = findViewById<TextView>(R.id.tvImageInfo)
+        when (requestCode) {
+
+            CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_FILE ->
+                if (resultCode == RESULT_OK)
+                    showImageFromFile(mImage, tvImageInfo, mFile!!)
+                else handleOther(resultCode)
+
+            CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE_BY_BITMAP ->
+                if (resultCode == RESULT_OK) {
+                    val extras = data!!.extras
+                    val imageBitmap = extras!!["data"] as Bitmap
+                    showImageFromBitmap(mImage, tvImageInfo, imageBitmap)
+                } else handleOther(resultCode)
+        }
+    }
+
+    private fun handleOther(resultCode: Int) {
+        if (resultCode == RESULT_CANCELED)
+            Toast.makeText(this, "Canceled...", Toast.LENGTH_LONG).show()
+        else Toast.makeText(this, "Picture NOT taken - unknown error...", Toast.LENGTH_LONG).show()
+    }
+
+
+    // show the image allocated in [f] in imageview [img]. Show meta data in [txt]
+    private fun showImageFromFile(img: ImageView, txt: TextView, f: File) {
+        img.setImageURI(Uri.fromFile(f))
+        img.setBackgroundColor(Color.RED)
+        //mImage.setRotation(90);
+        txt.text = "File at:" + f.absolutePath + " - size = " + f.length()
+
+    }
+
+    // show the image [bmap] in the imageview [img] - and put meta data in [txt]
+    private fun showImageFromBitmap(img: ImageView, txt: TextView, bmap: Bitmap) {
+        img.setImageBitmap(bmap)
+        //img.setLayoutParams(RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+        img.setBackgroundColor(Color.RED)
+        txt.text = "bitmap - size = " + bmap.byteCount
+
+    }
+}
